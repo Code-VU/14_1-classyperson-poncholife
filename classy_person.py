e28528edbdc55ce903dc00b309101c138d9014f2
Jonathan Willis <Jonathan.Willis@vu.com>
2023-05-21 10:17:53 -0500

    14.1with while loop

--- classy_person.py
+++ classy_person.py
@@ -9,6 +9,32 @@ don't forget the use of 'self' and to have the methods:
 
 class Person:
 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def increase_age(self):
+        self.age = self.age + 1
+
+    def say_greeting(self):
+        print('Hello world! My name is '+self.name+'!')
+
+    def count_to_age(self):
+        x = 1
+        while x < self.age:
+            print(x)
+            x = x+1
+
+# z = Person('Jonathan', 35)
+# z.increase_age()
+# z.say_greeting()
+# z.count_to_age()
+
+
+
+        
+    
+        
 
 
 
