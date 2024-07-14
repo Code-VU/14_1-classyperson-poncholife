52f74b7cece125de6118182422cd91a98b215e99
poncholife <jonathan.willis@veteransunited.com>
2024-07-14 14:10:32 -0500

    2nd course 2024 July 14

--- classy_person.py
+++ classy_person.py
@@ -9,33 +9,20 @@ don't forget the use of 'self' and to have the methods:
 
 class Person:
 
-    def __init__(self, name, age):
+    def __init__(self, age, name):
         self.name = name
         self.age = age
 
     def increase_age(self):
         self.age = self.age + 1
-
+    
     def say_greeting(self):
-        print('Hello world! My name is '+self.name+'!')
+        print(f'Hello world! My name is {self.name}!')
 
     def count_to_age(self):
-        x = 1
+        x = 0
         while x < self.age:
+            x = x + 1 
             print(x)
-            x = x+1
-
-# z = Person('Jonathan', 35)
-# z.increase_age()
-# z.say_greeting()
-# z.count_to_age()
-
-
-
-        
-    
-        
-
-
 
 # You won't need to call anything here.
